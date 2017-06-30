5148535d2f831e5436abfa52288d3a1c03126aa1
Pierre Fernbach <pierre.fernbach@laas.fr>
2017-06-30 15:47:37 +0200

    demo flatground ~ ok

--- script/tools/muscodSSH.py
+++ script/tools/muscodSSH.py
@@ -1,7 +1,16 @@
-cmd = "ssh -X iwaki 'cd /local/pfernbac/muscod/walk_dynamic/uc-dual/build ; source /local/pfernbac/config.sh ; muscod_release straight_walk_dynamic '"
+from planning.config import *
+
+
+cmd = "ssh -X iwaki 'cd "+MUSCOD_DIR+" ; source /local/pfernbac/config.sh ; muscod_release "+DAT_NAME+" ; killall pgxwin_server '"
+
+#cmd = "ssh -X iwaki 'cd /local/pfernbac/muscod/walk/uc-dual/build ; source /local/pfernbac/config.sh ; muscod_release straight_walk_planning ; killall pgxwin_server'"
 
 import subprocess
-subprocess.check_output(cmd,shell=True)
+try :
+  subprocess.check_output(cmd,shell=True)
+  print "close PGPLOT server to continue ..."
+except subprocess.CalledProcessError:
+  print "above error 'Error in `muscod_release'' is normal, only red ERROR message are an issue"
 
 
 
