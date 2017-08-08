f3b17cbffdad059c95e478cdd74f963687d1fea4
Pierre Fernbach <pierre.fernbach@laas.fr>
2017-08-08 10:11:16 +0200

    small changes in slalom script

--- script/dynamic/slalom_hyq_pathKino.py
+++ script/dynamic/slalom_hyq_pathKino.py
@@ -19,11 +19,11 @@ srdfSuffix = ""
 vMax = 1.;
 aMax = 5.;
 extraDof = 6
-mu=omniORB.any.to_any(MU)
+mu=omniORB.any.to_any(0.5)
 # Creating an instance of the helper class, and loading the robot
 rbprmBuilder = Builder ()
 rbprmBuilder.loadModel(urdfName, urdfNameRom, rootJointType, meshPackageName, packageName, urdfSuffix, srdfSuffix)
-rbprmBuilder.setJointBounds ("base_joint_xyz", [-6,6, -2.5, 2.5, 0.6, 0.65])
+rbprmBuilder.setJointBounds ("base_joint_xyz", [-5.5,5.5, -2.5, 2.5, 0.6, 0.65])
 # The following lines set constraint on the valid configurations:
 # a configuration is valid only if all limbs can create a contact ...
 rbprmBuilder.setFilter(['hyq_rhleg_rom', 'hyq_lfleg_rom', 'hyq_rfleg_rom','hyq_lhleg_rom'])
@@ -41,9 +41,9 @@ from hpp.corbaserver.rbprm.problem_solver import ProblemSolver
 ps = ProblemSolver( rbprmBuilder )
 ps.client.problem.setParameter("aMax",omniORB.any.to_any(aMax))
 ps.client.problem.setParameter("vMax",omniORB.any.to_any(vMax))
-ps.client.problem.setParameter("orientedPath",omniORB.any.to_any(1.))
+ps.client.problem.setParameter("orientedPath",omniORB.any.to_any(0.))
 ps.client.problem.setParameter("friction",mu)
-r = Viewer (ps)
+r = Viewer (ps,displayArrows = True)
 
 from hpp.corbaserver.affordance.affordance import AffordanceTool
 afftool = AffordanceTool ()
@@ -73,10 +73,11 @@ ps.setInitialConfig (q_init)
 ps.addGoalConfig (q_goal)
 # Choosing RBPRM shooter and path validation methods.
 ps.client.problem.selectConFigurationShooter("RbprmShooter")
-ps.client.problem.selectPathValidation("RbprmDynamicPathValidation",0.05)
+ps.client.problem.selectPathValidation("RbprmPathValidation",0.05)
 # Choosing kinodynamic methods : 
 ps.selectSteeringMethod("RBPRMKinodynamic")
 ps.selectDistance("KinodynamicDistance")
+ps.addPathOptimizer("RandomShortcutDynamic")
 ps.selectPathPlanner("DynamicPlanner")
 
 #solve the problem :
@@ -88,10 +89,10 @@ q_far = q_init[::]
 q_far[2] = -3
 r(q_far)
 
-r.solveAndDisplay("rm",1,0.01)
 
 
-#t = ps.solve ()
+
+
 
 
 
@@ -106,14 +107,14 @@ camera = [0.6293167471885681,
 r.client.gui.setCameraTransform(0,camera)
 """
 
-"""
-r.client.gui.removeFromGroup("rm",r.sceneName)
-r.client.gui.removeFromGroup("rmstart",r.sceneName)
-r.client.gui.removeFromGroup("rmgoal",r.sceneName)
-for i in range(0,ps.numberNodes()):
-  r.client.gui.removeFromGroup("vecRM"+str(i),r.sceneName)
 
-"""
+#t = ps.solve ()
+
+r.solveAndDisplay("rm",1,0.01)
+
+r.client.gui.removeFromGroup("rm_group",r.sceneName)
+
+
 
 
 
@@ -124,12 +125,28 @@ pp.dt=0.03
 pp.displayVelocityPath(0)
 r.client.gui.setVisibility("path_0_root","ALWAYS_ON_TOP")
 #display path
-pp.speed=0.3
+pp.speed=0.5
 #pp (0)
 
+
+
+###########################
 #display path with post-optimisation
 
 
+print("Press Enter to display the optimization ...")
+raw_input()
+i=0
+r.client.gui.removeFromGroup("path_0_root",r.sceneName)
+pp.displayVelocityPath(1)
+for i in range(1,10):
+  time.sleep(3)
+  ps.optimizePath(i)
+  r.client.gui.removeFromGroup("path_"+str(i)+"_root",r.sceneName)
+  pp.displayVelocityPath(i+1)
+
+
+###########################
 
 
 """
