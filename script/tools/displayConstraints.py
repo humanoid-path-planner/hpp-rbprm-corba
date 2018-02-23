c4f3c54fcfac703ed35e425aaaeddf9fd1283aab
Pierre Fernbach <pierre.fernbach@laas.fr>
2018-02-23 14:39:08 +0100

    [tools] add helper method to display constraints / trajectory

--- script/tools/displayConstraints.py
+++ script/tools/displayConstraints.py
@@ -7,9 +7,22 @@ s1,success = StateHelper.removeContact(s0,leg)
 
 r(s1.q())
 removeAllConstraints(r)
-pid = fullBody.isDynamicallyReachableFromState(s0.sId,s2.sId)
-pp.displayPath(pid,color=r.color.blue)
-r.client.gui.setVisibility('path_'+str(pid)+'_root','ALWAYS_ON_TOP')
+pid = fullBody.isDynamicallyReachableFromState(s0.sId,s2.sId,True)
+def showPath(pid):
+  if len(pid)==1:
+    pp.displayPath(int(pid[0]),color=r.color.blue)
+    r.client.gui.setVisibility('path_'+str(int(pid[0]))+'_root','ALWAYS_ON_TOP')
+  elif len(pid)==4:
+    pp.displayPath(int(pid[1]),color=r.color.green)
+    r.client.gui.setVisibility('path_'+str(int(pid[1]))+'_root','ALWAYS_ON_TOP')  
+    pp.displayPath(int(pid[2]),color=r.color.blue)
+    r.client.gui.setVisibility('path_'+str(int(pid[2]))+'_root','ALWAYS_ON_TOP')  
+    pp.displayPath(int(pid[3]),color=r.color.yellow)
+    r.client.gui.setVisibility('path_'+str(int(pid[3]))+'_root','ALWAYS_ON_TOP')
+  elif len(pid) == 3:
+    print "only two phases, not implemented yet."
+  else:
+    print "no path, test failed."
 
 
 
@@ -19,6 +32,7 @@ displayOneStepConstraints(r,True)
 r.startCapture ("capture/capture","png")
 r.stopCapture ()
 
+r(s0.q())
 fullBody.isReachableFromState(s0.sId,s1.sId)
 displayOneStepConstraints(r,True)
 
@@ -31,6 +45,7 @@ displayOneStepConstraints(r,True)
 r.startCapture ("capture/capture","png")
 r.stopCapture ()
 
+r(s2.q())
 fullBody.isReachableFromState(s1.sId,s2.sId)
 displayOneStepConstraints(r,True)
 
