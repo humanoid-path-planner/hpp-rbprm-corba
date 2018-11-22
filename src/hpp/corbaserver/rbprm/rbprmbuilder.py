4ab051a68eeba70dd0edc3ef81b3985d1838cee6
pFernbach <pierre.fernbach@gmail.com>
2018-11-22 11:12:15 +0100

    remove rbprm.problemSolver class and update rbprmBuilder to be used with corbaserver.ProblemSolver

--- src/hpp/corbaserver/rbprm/rbprmbuilder.py
+++ src/hpp/corbaserver/rbprm/rbprmbuilder.py
@@ -20,16 +20,6 @@ from hpp.corbaserver.rbprm import Client as RbprmClient
 from hpp.corbaserver import Client as BasicClient
 import hpp.gepetto.blender.exportmotion as em
 
-## Corba clients to the various servers
-#
-class CorbaClient:
-    """
-    Container for corba clients to various interfaces.
-    """
-    def __init__ (self):
-        self.basic = BasicClient ()
-        self.rbprm = RbprmClient ()
-
 ## Load and handle a RbprmDevice robot for rbprm planning
 #
 #  A RbprmDevice robot is a dual representation of a robots. One robot describes the 
@@ -37,10 +27,11 @@ class CorbaClient:
 class Builder (object):
     ## Constructor
     def __init__ (self, load = True):
-        self.tf_root = "base_link"
-        self.rootJointType = dict()
-        self.client = CorbaClient ()
-        self.load = load
+				self.tf_root = "base_link"
+				self.rootJointType = dict()
+				self.client = BasicClient () # client of hpp.corbaserver.robot
+				self.clientRbprm = RbprmClient () # client of hpp.corbaserver.rbprm.rbprmBuilder
+				self.load = load
         
     ## Virtual function to load the robot model.
 	#
@@ -55,18 +46,18 @@ class Builder (object):
     def loadModel (self, urdfName, urdfNameroms, rootJointType, meshPackageName, packageName, urdfSuffix, srdfSuffix):
 		if(isinstance(urdfNameroms, list)):		
 			for urdfNamerom in urdfNameroms:
-				self.client.rbprm.rbprm.loadRobotRomModel(urdfNamerom, rootJointType, packageName, urdfNamerom, urdfSuffix, srdfSuffix)
+				self.clientRbprm.rbprm.loadRobotRomModel(urdfNamerom, rootJointType, packageName, urdfNamerom, urdfSuffix, srdfSuffix)
 		else:
-			self.client.rbprm.rbprm.loadRobotRomModel(urdfNameroms, rootJointType, packageName, urdfNameroms, urdfSuffix, srdfSuffix)
-		self.client.rbprm.rbprm.initNewProblemSolver()
-		self.client.rbprm.rbprm.loadRobotCompleteModel(urdfName, rootJointType, packageName, urdfName, urdfSuffix, srdfSuffix)		
+			self.clientRbprm.rbprm.loadRobotRomModel(urdfNameroms, rootJointType, packageName, urdfNameroms, urdfSuffix, srdfSuffix)
+		self.clientRbprm.rbprm.initNewProblemSolver()
+		self.clientRbprm.rbprm.loadRobotCompleteModel(urdfName, rootJointType, packageName, urdfName, urdfSuffix, srdfSuffix)
 		self.name = urdfName
 		self.displayName = urdfName
 		self.tf_root = "base_link"
 		self.rootJointType = rootJointType
-		self.jointNames = self.client.basic.robot.getJointNames ()
-		self.allJointNames = self.client.basic.robot.getAllJointNames ()
-		self.client.basic.robot.meshPackageName = meshPackageName
+		self.jointNames = self.client.robot.getJointNames ()
+		self.allJointNames = self.client.robot.getAllJointNames ()
+		self.client.robot.meshPackageName = meshPackageName
 		self.meshPackageName = meshPackageName
 		self.rankInConfiguration = dict ()
 		self.rankInVelocity = dict ()
@@ -77,22 +68,22 @@ class Builder (object):
 		rankInConfiguration = rankInVelocity = 0
 		for j in self.jointNames:
 			self.rankInConfiguration [j] = rankInConfiguration
-			rankInConfiguration += self.client.basic.robot.getJointConfigSize (j)
+			rankInConfiguration += self.client.robot.getJointConfigSize (j)
 			self.rankInVelocity [j] = rankInVelocity
-			rankInVelocity += self.client.basic.robot.getJointNumberDof (j)
+			rankInVelocity += self.client.robot.getJointNumberDof (j)
 
 
 
 	## Init RbprmShooter
     #
     def initshooter (self):
-        return self.client.rbprm.rbprm.initshooter ()
+				return self.clientRbprm.rbprm.initshooter ()
 
 	## Sets limits on robot orientation, described according to Euler's ZYX rotation order
 	#
     # \param bounds 6D vector with the lower and upperBound for each rotation axis in sequence
     def boundSO3 (self,  bounds):
-        return self.client.rbprm.rbprm.boundSO3 (bounds)
+				return self.clientRbprm.rbprm.boundSO3 (bounds)
 
 	## Specifies a preferred affordance for a given rom.
 	# This constrains the planner to accept a rom configuration only if
@@ -101,7 +92,7 @@ class Builder (object):
     # \param rom name of the rome,
     # \param affordances list of affordance names
     def setAffordanceFilter (self, rom, affordances):
-        return self.client.rbprm.rbprm.setAffordanceFilter (rom, affordances)
+				return self.clientRbprm.rbprm.setAffordanceFilter (rom, affordances)
 
 	## Specifies a rom constraint for the planner.
 	# A configuration will be valid if and only if the considered rom collides
@@ -109,7 +100,7 @@ class Builder (object):
 	#
     # \param romFilter array of roms indicated by name, which determine the constraint.
     def setFilter (self, romFilter):
-        return self.client.rbprm.rbprm.setFilter (romFilter)
+				return self.clientRbprm.rbprm.setFilter (romFilter)
 
 	## Export a computed path for blender
     #
@@ -118,7 +109,7 @@ class Builder (object):
 	# \param pathId if of the considered path
 	# \param filename name of the output file where to save the output
     def exportPath (self, viewer, problem, pathId, stepsize, filename):
-		em.exportPath(viewer, self.client.basic.robot, problem, pathId, stepsize, filename)
+		em.exportPath(viewer, self.client.robot, problem, pathId, stepsize, filename)
 
    ## \name Degrees of freedom
     #  \{
@@ -126,12 +117,12 @@ class Builder (object):
     ## Get size of configuration
     # \return size of configuration
     def getConfigSize (self):
-        return self.client.basic.robot.getConfigSize ()
+				return self.client.robot.getConfigSize ()
 
     # Get size of velocity
     # \return size of velocity
     def getNumberDof (self):
-        return self.client.basic.robot.getNumberDof ()
+				return self.client.robot.getNumberDof ()
     ## \}
 
     ## \name Joints
@@ -139,41 +130,41 @@ class Builder (object):
 
     ## Get joint names in the same order as in the configuration.
     def getJointNames (self):
-        return self.client.basic.robot.getJointNames ()
+				return self.client.robot.getJointNames ()
 
     ## Get joint names in the same order as in the configuration.
     def getAllJointNames (self):
-        return self.client.basic.robot.getAllJointNames ()
+				return self.client.robot.getAllJointNames ()
 
     ## Get joint position.
     def getJointPosition (self, jointName):
-        return self.client.basic.robot.getJointPosition (jointName)
+				return self.client.robot.getJointPosition (jointName)
 
     ## Set static position of joint in its parent frame
     def setJointPosition (self, jointName, position):
-        return self.client.basic.robot.setJointPosition (jointName, position)
+				return self.client.robot.setJointPosition (jointName, position)
 
     ## Get joint number degrees of freedom.
     def getJointNumberDof (self, jointName):
-        return self.client.basic.robot.getJointNumberDof (jointName)
+				return self.client.robot.getJointNumberDof (jointName)
 
     ## Get joint number config size.
     def getJointConfigSize (self, jointName):
-        return self.client.basic.robot.getJointConfigSize (jointName)
+				return self.client.robot.getJointConfigSize (jointName)
 
     ## set bounds for the joint
     def setJointBounds (self, jointName, inJointBound):
-        return self.client.basic.robot.setJointBounds (jointName, inJointBound)
+				return self.client.robot.setJointBounds (jointName, inJointBound)
 
     ## Set bounds on the translation part of the freeflyer joint.
     #
     #  Valid only if the robot has a freeflyer joint.
     def setTranslationBounds (self, xmin, xmax, ymin, ymax, zmin, zmax):
-        self.client.basic.robot.setJointBounds \
+				self.client.robot.setJointBounds \
             (self.displayName + "base_joint_x", [xmin, xmax])
-        self.client.basic.robot.setJointBounds \
+				self.client.robot.setJointBounds \
             (self.displayName + "base_joint_y", [ymin, ymax])
-        self.client.basic.robot.setJointBounds \
+				self.client.robot.setJointBounds \
             (self.displayName + "base_joint_z", [zmin, zmax])
 
     ## Get link position in joint frame
@@ -186,17 +177,17 @@ class Builder (object):
     # \param jointName name of the joint
     # \return position of the link in world frame.
     def getLinkPosition (self, jointName):
-        return self.client.basic.robot.getLinkPosition (jointName)
+				return self.client.robot.getLinkPosition (jointName)
 
     ## Get link name
     #
     # \param jointName name of the joint,
     # \return name of the link.
     def getLinkName (self, jointName):
-        return self.client.basic.robot.getLinkName (jointName)
+				return self.client.robot.getLinkName (jointName)
         
     def getLinkNames (self, jointName):
-        return self.client.basic.robot.getLinkNames (jointName)
+				return self.client.robot.getLinkNames (jointName)
     ## \}
 
     ## \name Access to current configuration
@@ -206,18 +197,18 @@ class Builder (object):
     #
     #  \param q configuration of the composite robot
     def setCurrentConfig (self, q):
-        self.client.basic.robot.setCurrentConfig (q)
+				self.client.robot.setCurrentConfig (q)
 
     ## Get current configuration of composite robot
     #
     #  \return configuration of the composite robot
     def getCurrentConfig (self):
-        return self.client.basic.robot.getCurrentConfig ()
+				return self.client.robot.getCurrentConfig ()
 
     ## Shoot random configuration
     #  \return dofArray Array of degrees of freedom.
     def shootRandomConfig(self):
-        return self.client.basic.robot.shootRandomConfig ()
+				return self.client.robot.shootRandomConfig ()
 
     ## \}
 
@@ -228,20 +219,20 @@ class Builder (object):
     #  \param inJointName name of the joint.
     #  \return list of names of CollisionObject attached to the body.
     def getJointInnerObjects (self, jointName):
-        return self.client.basic.robot.getJointInnerObjects (jointName)
+				return self.client.robot.getJointInnerObjects (jointName)
 
 
     ##  Get list of collision objects tested with the body attached to a joint
     #  \param inJointName name of the joint.
     #  \return list of names of CollisionObject
     def getJointOuterObjects (self, jointName):
-        return self.client.basic.robot.getJointOuterObjects (jointName)
+				return self.client.robot.getJointOuterObjects (jointName)
 
     ## Get position of robot object
     #  \param objectName name of the object.
     #  \return transformation as a hpp.Transform object
     def getObjectPosition (self, objectName):
-        return Transform (self.client.basic.robot.getObjectPosition
+				return Transform (self.client.robot.getObjectPosition
                           (objectName))
 
     ## \brief Remove an obstacle from outer objects of a joint body
@@ -252,7 +243,7 @@ class Builder (object):
     #  \param distance whether distance to object should be computed.
     def removeObstacleFromJoint (self, objectName, jointName, collision,
                                  distance):
-        return self.client.basic.obstacle.removeObstacleFromJoint \
+				return self.client.obstacle.removeObstacleFromJoint \
             (objectName, jointName, collision, distance)
 
 
@@ -268,8 +259,8 @@ class Builder (object):
     # \return whether configuration is valid
     # \note Deprecated. Use isConfigValid instead.
     def collisionTest (self):
-        print "Deprecated. Use isConfigValid instead"
-        return self.client.basic.robot.collisionTest ()
+			print "Deprecated. Use isConfigValid instead"
+			return self.client.robot.collisionTest ()
 
     ## Check the validity of a configuration.
     #
@@ -277,7 +268,7 @@ class Builder (object):
     # \param cfg a configuration
     # \return whether configuration is valid
     def isConfigValid (self, cfg):
-        return self.client.basic.robot.isConfigValid (cfg)
+				return self.client.robot.isConfigValid (cfg)
     
     ## Compute distances between bodies and obstacles
     #
@@ -289,7 +280,7 @@ class Builder (object):
     # \note outer objects for a body can also be inner objects of another
     # body.
     def distancesToCollision (self):
-        return self.client.basic.robot.distancesToCollision ()
+				return self.client.robot.distancesToCollision ()
     ## \}
 
     ## \}
@@ -298,21 +289,21 @@ class Builder (object):
 
     ## Get mass of robot
     def getMass (self):
-        return self.client.basic.robot.getMass ()
+				return self.client.robot.getMass ()
 
     ## Get position of center of mass
     def getCenterOfMass (self):
-        return self.client.basic.robot.getCenterOfMass ()
+				return self.client.robot.getCenterOfMass ()
     ## Get Jacobian of the center of mass
     def getJacobianCenterOfMass (self):
-        return self.client.basic.robot.getJacobianCenterOfMass ()
+				return self.client.robot.getJacobianCenterOfMass ()
     ##\}
 
     ## Get the dimension of the extra configuration space
     def getDimensionExtraConfigSpace(self):
-        return self.client.basic.robot.getDimensionExtraConfigSpace()
+				return self.client.robot.getDimensionExtraConfigSpace()
 
     ## Convert a direction vector to a quaternion (use Eigen::Quaterniond::FromTwoVectors with Z vector)
     # \param u the vector director
     def quaternionFromVector(self,vector):
-        return self.client.basic.robot.quaternionFromVector(vector)
+				return self.client.robot.quaternionFromVector(vector)
