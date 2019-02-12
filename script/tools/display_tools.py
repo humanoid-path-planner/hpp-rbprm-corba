8a057f97f34fd784abf161a0929095a3984e44d4
pFernbach <pierre.fernbach@gmail.com>
2019-02-12 12:17:17 +0100

    merge tools.py and display_tools.py scripts

--- script/tools/display_tools.py
+++ script/tools/display_tools.py
@@ -16,3 +16,25 @@ def displayContactSequence(r,configs,pause=1.):
   for i in range(0,len(configs)):
     r(configs[i])
     time.sleep(pause)	
+
+def moveObject(viewer,pos,rotation=[1,0,0,0]):
+  viewer.client.gui.applyConfiguration(name,pos+rotation)
+  viewer.client.gui.refresh()
+
+def addVector(viewer,rbprmBuilder,color,v,name=None):
+  gui = viewer.client.gui
+  if name==None:
+    i=0
+    name='vector_'+str(i)
+    while name in gui.getNodeList():
+      i=i+1
+      name='sphere_'+str(i)
+  quat = rbprmBuilder.quaternionFromVector(v[3:6])
+  v[3:7] = quat[::]
+  gui.addArrow(name,0.02,1,color)
+  gui.addToGroup(name,viewer.sceneName)
+  gui.setVisibility(name,"ON")
+  gui.applyConfiguration(name,v)
+  gui.refresh()
+
+
