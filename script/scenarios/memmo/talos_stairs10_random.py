6e11c4f6495cec8201d0e1ff41d41464ca99ec01
pFernbach <pierre.fernbach@gmail.com>
2019-05-31 20:01:22 +0200

    [script] add scripts for talos on 10cm stairs

---
+++ script/scenarios/memmo/talos_stairs10_random.py
@@ -0,0 +1,150 @@
+from hpp.corbaserver.rbprm.talos import Robot
+from hpp.gepetto import Viewer
+from tools.display_tools import *
+import time
+print "Plan guide trajectory ..."
+import talos_stairs10_random_path as tp
+print "Done."
+import time
+statusFilename = tp.statusFilename
+pId = 0
+f = open(statusFilename,"a")
+if tp.ps.numberPaths() > 0 :
+  print "Path planning OK."
+  f.write("Planning_success: True"+"\n")
+  f.close()
+else :
+  print "Error during path planning"
+  f.write("Planning_success: False"+"\n")
+  f.close()
+  import sys
+  sys.exit(1)
+
+
+
+pId = tp.pId
+fullBody = Robot ()
+# Set the bounds for the root, take slightly larger bounding box than during root planning
+root_bounds = tp.root_bounds[::]
+root_bounds[0] -= 0.2
+root_bounds[1] += 0.2
+root_bounds[2] -= 0.2
+root_bounds[3] += 0.2
+root_bounds[-1] = 1.9
+root_bounds[-2] = 0.8
+fullBody.setJointBounds ("root_joint",  root_bounds)
+# add the 6 extraDof for velocity and acceleration (see *_path.py script)
+fullBody.client.robot.setDimensionExtraConfigSpace(tp.extraDof)
+fullBody.client.robot.setExtraConfigSpaceBounds([-tp.vMax,tp.vMax,-tp.vMax,tp.vMax,-10.,10.,-tp.aMax,tp.aMax,-tp.aMax,tp.aMax,-10.,10.])
+ps = tp.ProblemSolver( fullBody )
+ps.setParameter("Kinodynamic/velocityBound",tp.vMax)
+ps.setParameter("Kinodynamic/accelerationBound",tp.aMax)
+#load the viewer
+try :
+    v = tp.Viewer (ps,viewerClient=tp.v.client, displayCoM = True)
+except Exception:
+    print "No viewer started !"
+    class FakeViewer():
+        def __init__(self):
+            return
+        def __call__(self,q):
+            return
+        def addLandmark(self,a,b):
+            return
+    v = FakeViewer()
+
+
+# load a reference configuration
+q_ref = fullBody.referenceConfig[::]+[0,0,0,0,0,0]
+q_init = q_ref[::]
+fullBody.setReferenceConfig(q_ref)
+fullBody.setCurrentConfig (q_init)
+fullBody.setPostureWeights(fullBody.postureWeights[::]+[0]*6)
+fullBody.usePosturalTaskContactCreation(True)
+print "Generate limb DB ..."
+tStart = time.time()
+# generate databases : 
+nbSamples = 50000
+heuristic="fixedStep06"
+fullBody.addLimb(fullBody.rLegId,fullBody.rleg,fullBody.rfoot,fullBody.rLegOffset,fullBody.rLegNormal, fullBody.rLegx, fullBody.rLegy, nbSamples, heuristic, 0.01,kinematicConstraintsPath=fullBody.rLegKinematicConstraints,kinematicConstraintsMin = 0.85)
+fullBody.addLimb(fullBody.lLegId,fullBody.lleg,fullBody.lfoot,fullBody.lLegOffset,fullBody.rLegNormal, fullBody.lLegx, fullBody.lLegy, nbSamples, heuristic, 0.01,kinematicConstraintsPath=fullBody.lLegKinematicConstraints,kinematicConstraintsMin = 0.85)
+#fullBody.runLimbSampleAnalysis(fullBody.rLegId, "ReferenceConfiguration", True)
+#fullBody.runLimbSampleAnalysis(fullBody.lLegId, "ReferenceConfiguration", True)
+
+
+tGenerate =  time.time() - tStart
+print "Done."
+print "Databases generated in : "+str(tGenerate)+" s"
+
+
+#define initial and final configurations : 
+configSize = fullBody.getConfigSize() -fullBody.client.robot.getDimensionExtraConfigSpace()
+
+q_init[0:7] = tp.ps.configAtParam(pId,0.)[0:7] # use this to get the correct orientation
+q_goal = q_init[::]; q_goal[0:7] = tp.ps.configAtParam(pId,tp.ps.pathLength(pId))[0:7]
+dir_init = tp.ps.configAtParam(pId,0.)[tp.indexECS:tp.indexECS+3]
+acc_init = tp.ps.configAtParam(pId,0.)[tp.indexECS+3:tp.indexECS+6]
+dir_goal = tp.ps.configAtParam(pId,tp.ps.pathLength(pId))[tp.indexECS:tp.indexECS+3]
+acc_goal = [0,0,0]
+
+robTreshold = 2
+# copy extraconfig for start and init configurations
+q_init[configSize:configSize+3] = dir_init[::]
+q_init[configSize+3:configSize+6] = acc_init[::]
+q_goal[configSize:configSize+3] = dir_goal[::]
+q_goal[configSize+3:configSize+6] = [0,0,0]
+
+q_init[2]+=0.0432773
+q_goal[2]+=0.0432773
+
+fullBody.setStaticStability(True)
+fullBody.setCurrentConfig (q_init)
+v(q_init)
+
+fullBody.setCurrentConfig (q_goal)
+v(q_goal)
+
+v.addLandmark('talos/base_link',0.3)
+v(q_init)
+
+# specify the full body configurations as start and goal state of the problem
+fullBody.setStartState(q_init,[fullBody.lLegId,fullBody.rLegId])
+fullBody.setEndState(q_goal,[fullBody.lLegId,fullBody.rLegId])
+
+
+print "Generate contact plan ..."
+tStart = time.time()
+configs = fullBody.interpolate(0.005,pathId=pId,robustnessTreshold = 2, filterStates = True,testReachability=True,quasiStatic=True)
+tInterpolateConfigs = time.time() - tStart
+print "Done."
+print "number of configs :", len(configs)
+
+if len(configs) < 8 :
+  cg_success = False
+  print "Error during contact generation."
+else:
+  cg_success = True
+  print "Contact generation Done."
+if abs(configs[-1][0] - tp.q_goal[0]) < 0.01 and abs(configs[-1][1]- tp.q_goal[1]) < 0.01  and (len(fullBody.getContactsVariations(len(configs)-2,len(configs)-1))==1):
+  print "Contact generation successful."
+  cg_reach_goal = True
+else:
+  print "Contact generation failed to reach the goal."
+  cg_reach_goal = False
+if len(configs) > 30 :
+  cg_too_many_states = True
+  cg_success = False
+  print "Discarded contact sequence because it was too long."
+else:
+  cg_too_many_states = False
+
+f = open(statusFilename,"a")
+f.write("cg_success: "+str(cg_success)+"\n")
+f.write("cg_reach_goal: "+str(cg_reach_goal)+"\n")
+f.write("cg_too_many_states: "+str(cg_too_many_states)+"\n")
+f.close()
+
+if (not cg_success) or cg_too_many_states:
+  import sys
+  sys.exit(1)
+
---
+++ script/scenarios/memmo/talos_stairs10_random_path.py
@@ -0,0 +1,181 @@
+from hpp.corbaserver.rbprm.talos_abstract import Robot
+from hpp.gepetto import Viewer
+from hpp.corbaserver import Client
+from hpp.corbaserver import ProblemSolver
+import time
+statusFilename = "infos.log"
+Robot.urdfName+="_large"
+
+vMax = 0.3# linear velocity bound for the root
+aMax = 0.05 # linear acceleration bound for the root
+extraDof = 6
+mu=0.5# coefficient of friction
+# Creating an instance of the helper class, and loading the robot
+rbprmBuilder = Robot()
+# Define bounds for the root : bounding box of the scenario
+root_bounds = [-0.9,2.55,-0.13,2., 0.98, 1.6]
+rbprmBuilder.setJointBounds ("root_joint", root_bounds)
+# As this scenario only consider walking, we fix the DOF of the torso :
+rbprmBuilder.setJointBounds ('torso_1_joint', [0,0])
+rbprmBuilder.setJointBounds ('torso_2_joint', [0.,0.])
+
+# The following lines set constraint on the valid configurations:
+# a configuration is valid only if all limbs can create a contact with the corresponding afforcances type
+rbprmBuilder.setFilter(['talos_lleg_rom','talos_rleg_rom'])
+rbprmBuilder.setAffordanceFilter('talos_lleg_rom', ['Support',])
+rbprmBuilder.setAffordanceFilter('talos_rleg_rom', ['Support'])
+# We also bound the rotations of the torso. (z, y, x)
+rbprmBuilder.boundSO3([-4.,4.,-0.1,0.1,-0.1,0.1])
+# Add 6 extraDOF to the problem, used to store the linear velocity and acceleration of the root
+rbprmBuilder.client.robot.setDimensionExtraConfigSpace(extraDof)
+# We set the bounds of this extraDof with velocity and acceleration bounds (expect on z axis)
+rbprmBuilder.client.robot.setExtraConfigSpaceBounds([-vMax,vMax,-vMax,vMax,-10.,10.,-aMax,aMax,-aMax,aMax,-10.,10.])
+indexECS = rbprmBuilder.getConfigSize() - rbprmBuilder.client.robot.getDimensionExtraConfigSpace()
+
+# Creating an instance of HPP problem solver
+ps = ProblemSolver( rbprmBuilder )
+# define parameters used by various methods : 
+ps.setParameter("Kinodynamic/velocityBound",vMax)
+ps.setParameter("Kinodynamic/accelerationBound",aMax)
+# force the orientation of the trunk to match the direction of the motion
+ps.setParameter("Kinodynamic/forceOrientation",True)
+ps.setParameter("DynamicPlanner/sizeFootX",0.2)
+ps.setParameter("DynamicPlanner/sizeFootY",0.12)
+ps.setParameter("DynamicPlanner/friction",mu)
+# sample only configuration with null velocity and acceleration :
+ps.setParameter("ConfigurationShooter/sampleExtraDOF",False)
+ps.setParameter("PathOptimization/RandomShortcut/NumberOfLoops",100)
+
+# initialize the viewer :
+from hpp.gepetto import ViewerFactory
+vf = ViewerFactory (ps)
+
+# load the module to analyse the environnement and compute the possible contact surfaces
+from hpp.corbaserver.affordance.affordance import AffordanceTool
+afftool = AffordanceTool ()
+afftool.setAffordanceConfig('Support', [0.5, 0.03, 0.00005])
+afftool.loadObstacleModel ("hpp_environments", "multicontact/bauzil_stairs", "planning", vf,reduceSizes=[0.18,0.,0.])
+#load the viewer
+try :
+    v = vf.createViewer(displayArrows = True)
+except Exception:
+    print "No viewer started !"
+    class FakeViewer():
+        sceneName = ""
+        def __init__(self):
+            return
+        def __call__(self,q):
+            return
+        def addLandmark(self,a,b):
+            return
+    v = FakeViewer()
+#afftool.visualiseAffordances('Support', v, v.color.lightBrown)
+v.addLandmark(v.sceneName,1)
+
+#####
+import random
+import numpy as np
+from pinocchio import Quaternion
+from tools.sampleRotation import sampleRotationForConfig
+"""
+# rectangle used to sample position of the floor
+x_floor=[-0.9,0.05]
+y_floor=[0.6,2.]
+z_floor=0.98
+# rectangle used to sample position on the plateform
+x_plat=[1.9,2.55]
+y_plat=[-0.13,1.53]
+z_plat=1.58
+"""
+# rectangle used to sample position of the floor
+x_floor=[-0.9,0.05]
+y_floor=[0.8,1.50]
+z_floor=0.98
+# rectangle used to sample position on the plateform
+x_plat=[1.9,2.55]
+y_plat=[0.8,1.50]
+z_plat=1.58
+vPredef = 0.05
+vx = np.matrix([1,0,0]).T
+#####
+
+
+q_up = rbprmBuilder.getCurrentConfig ()
+q_down = q_up[::]
+#generate a random problem : (q_init, q_goal)
+random.seed()
+go_up = random.randint(0,1)
+if go_up:
+    print "go upstair"
+    alphaBounds=[np.pi/4., 3.*np.pi/4.]
+else :
+    print "go downstair"
+    alphaBounds=[5.*np.pi/4., 7.*np.pi/4.]
+
+# sample random valid position on the floor : 
+while not rbprmBuilder.isConfigValid(q_down)[0]:
+    q_down[0] = random.uniform(x_floor[0],x_floor[1])
+    q_down[1] = random.uniform(y_floor[0],y_floor[1])
+    q_down[2] = z_floor
+    # sample random orientation : 
+    q_down = sampleRotationForConfig(alphaBounds,q_down,vPredef)
+# sample random valid position on the platform : 
+while not rbprmBuilder.isConfigValid(q_up)[0]:
+    q_up[0] = random.uniform(x_plat[0],x_plat[1])
+    q_up[1] = random.uniform(y_plat[0],y_plat[1])
+    q_up[2] = z_plat
+    # sample random orientation : 
+    q_up = sampleRotationForConfig(alphaBounds,q_up,vPredef)
+
+
+if go_up:
+    q_init = q_down
+    q_goal = q_up
+else :
+    q_init = q_up
+    q_goal = q_down
+
+# write problem in files : 
+f = open(statusFilename,"w")
+f.write("q_init= "+str(q_init)+"\n")
+f.write("q_goal= "+str(q_goal)+"\n")
+f.close()
+
+
+ps.setInitialConfig (q_init)
+v(q_init)
+ps.addGoalConfig (q_goal)
+v(q_goal)
+
+# Choosing RBPRM shooter and path validation methods.
+ps.selectConfigurationShooter("RbprmShooter")
+ps.addPathOptimizer ("RandomShortcutDynamic")
+ps.selectPathValidation("RbprmPathValidation",0.05)
+# Choosing kinodynamic methods :
+ps.selectSteeringMethod("RBPRMKinodynamic")
+ps.selectDistance("Kinodynamic")
+ps.selectPathPlanner("DynamicPlanner")
+
+# Solve the planning problem :
+
+t = ps.solve ()
+print "Guide planning time : ",t
+#pId = ps.numberPaths()-1
+pId = 0
+
+# display solution :
+try : 
+    from hpp.gepetto import PathPlayer
+    pp = PathPlayer (v)
+    pp.dt=0.1
+    pp.displayVelocityPath(pId)
+    v.client.gui.setVisibility("path_0_root","ALWAYS_ON_TOP")
+    pp.dt = 0.01
+    pp(pId)
+except Exception:
+    pass
+# move the robot out of the view before computing the contacts
+q_far = q_init[::]
+q_far[2] = -2
+v(q_far)
+
