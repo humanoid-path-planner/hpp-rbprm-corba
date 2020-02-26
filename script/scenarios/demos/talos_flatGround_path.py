d6c5cee5dbd07256e4c520d780ecd491f990249f
Pierre Fernbach <pierre.fernbach@laas.fr>
2020-02-26 16:16:50 +0100

    [Script] encapsulate talos_flatGround_path in class

--- script/scenarios/demos/talos_flatGround_path.py
+++ script/scenarios/demos/talos_flatGround_path.py
@@ -1,15 +1,19 @@
 from scenarios.template_talos_path import TalosPathPlanning
 
-path_planning = TalosPathPlanning()
-path_planning.init_problem()
+class PathPlanning(TalosPathPlanning):
 
-path_planning.q_init[0:2] = [0, 0]
-path_planning.q_goal[0:2] = [1, 0]
+    def run(self):
+        self.init_problem()
 
-path_planning.init_viewer("multicontact/ground", visualize_affordances=["Support"])
-path_planning.init_planner()
-path_planning.solve()
+        self.q_init[0:2] = [0, 0]
+        self.q_goal[0:2] = [1, 0]
 
-path_planning.display_path()
+        self.init_viewer("multicontact/ground", visualize_affordances=["Support"])
+        self.init_planner()
+        self.solve()
+        self.display_path()
+        self.play_path()
 
-path_planning.play_path()
+
+if __name__ == "__main__":
+    PathPlanning().run()
