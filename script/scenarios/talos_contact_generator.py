74bc572590f592199fb2d79ba25f7138695f3938
Pierre Fernbach <pierre.fernbach@laas.fr>
2020-03-03 11:15:38 +0100

    [Script] add talos/hrp2 template scripts

---
+++ script/scenarios/talos_contact_generator.py
@@ -0,0 +1,19 @@
+from .abstract_contact_generator import AbstractContactGenerator
+import time
+
+class TalosContactGenerator(AbstractContactGenerator):
+
+    def __init__(self, path_planner):
+        super().__init__(path_planner)
+        self.robustness = 2
+
+
+    def load_fullbody(self):
+        from talos_rbprm.talos import Robot
+        self.fullbody = Robot()
+        self.q_ref = self.fullbody.referenceConfig[::] + [0] * self.path_planner.extra_dof
+        self.weight_postural = self.fullbody.postureWeights[::] + [0] * self.path_planner.extra_dof
+
+    def init_viewer(self):
+        super().init_viewer()
+        self.v.addLandmark('talos/base_link', 0.3)
