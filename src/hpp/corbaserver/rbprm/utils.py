82d8ca0f2d54c1be5c08e99c9fae81f0ed2caadc
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-06-08 15:08:51 +0200

    [Python] ServerManager is now available in hpp-corbaserver

--- src/hpp/corbaserver/rbprm/utils.py
+++ src/hpp/corbaserver/rbprm/utils.py
@@ -4,35 +4,41 @@
 import os
 import subprocess
 import time
+import warnings
 
 try:
-    from subprocess import DEVNULL, run
-except ImportError:  # Python2 fallback
-    DEVNULL = os.open(os.devnull, os.O_RDWR)
+    from hpp.utils import ServerManager
+    warnings.warn('Please import ServerManager directly from hpp.utils', DeprecationWarning)
+except ImportError:  # hpp-corbaserver < 4.9.1 fallback
 
-    def run(*args):
-        subprocess.Popen(*args).wait()
+    try:
+        from subprocess import DEVNULL, run
+    except ImportError:  # Python2 fallback
+        DEVNULL = os.open(os.devnull, os.O_RDWR)
+
+        def run(*args):
+            subprocess.Popen(*args).wait()
 
 
-class ServerManager:
-    """A context to ensure a server is running."""
-    def __init__(self, server):
-        self.server = server
-        run(['killall', self.server])
+    class ServerManager:
+        """A context to ensure a server is running."""
+        def __init__(self, server):
+            self.server = server
+            run(['killall', self.server])
 
-    def __enter__(self):
-        """Run the server in background
+        def __enter__(self):
+            """Run the server in background
 
-        stdout and stderr outputs of the child process are redirected to devnull (hidden).
-        preexec_fn is used to ignore ctrl-c signal send to the main script
-        (otherwise they are forwarded to the child process)
-        """
-        self.process = subprocess.Popen(self.server,
-                                        stdout=DEVNULL,
-                                        stderr=DEVNULL,
-                                        preexec_fn=os.setpgrp)
-        # give it some time to start
-        time.sleep(3)
+            stdout and stderr outputs of the child process are redirected to devnull (hidden).
+            preexec_fn is used to ignore ctrl-c signal send to the main script
+            (otherwise they are forwarded to the child process)
+            """
+            self.process = subprocess.Popen(self.server,
+                                            stdout=DEVNULL,
+                                            stderr=DEVNULL,
+                                            preexec_fn=os.setpgrp)
+            # give it some time to start
+            time.sleep(3)
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.process.kill()
+        def __exit__(self, exc_type, exc_value, exc_traceback):
+            self.process.kill()
